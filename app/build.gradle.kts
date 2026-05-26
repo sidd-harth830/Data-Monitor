9a35965226904f117d07534c35ad1526127e9e2c
Siddharth <leocarnivas@gmail.com>
2026-05-26 09:53:14 +0530

    remove signInConfig block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,22 +20,14 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  signingConfigs {
-    create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
-    }
-  }
-
   buildTypes {
     release {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
+    }
+    debug {
+      // Android will automatically handle debug signing. Do not add anything here.
     }
   }
   
