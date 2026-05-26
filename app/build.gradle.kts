68a755b9aede522482a4fc0d5c7ce4f64a4a9203
Siddharth <leocarnivas@gmail.com>
2026-05-26 09:20:57 +0530

    Updated package ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.datainfo.yqzplm"
+    applicationId = "com.siddharth.datamonitor"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "1.0"
+    versionName = "1.0.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
